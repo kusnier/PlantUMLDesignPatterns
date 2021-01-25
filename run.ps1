7d85c993034ee978eb108b193ad1da9530b0f2dd
Sebastian Kusnier <sebastian@kusnier.net>
2021-01-25 10:33:05 +0100

    Add a powershell script for generation of the png files

---
+++ run.ps1
@@ -0,0 +1,23 @@
+#!/usr/bin/pwsh
+
+$DataHashes = @{}
+
+if (Test-Path data.json) {
+    $DataHashes = Get-Content data.json | ConvertFrom-Json
+}
+
+Get-ChildItem -Filter *.plantuml | ForEach-Object {
+    $CurrentFile = $_.FullName
+    $CurrentFileName = $_.Name
+    $TargetFile = $_.Name -replace '.plantuml', '.png'
+    $CurrentHash =  $(Get-FileHash $CurrentFile -Algorithm MD5).Hash
+    $GeneratedHash = $DataHashes."$CurrentFileName"
+    $FileHasChanged = $CurrentHash -ne $GeneratedHash
+    $FileNotExisting = Test-Path $TargetFile
+
+    if ($FileHasChanged -or $FileNotExisting) {
+        java -jar plantuml.jar -duration -o output $CurrentFile
+        $DataHashes | Add-Member -Force -NotePropertyName $CurrentFileName -NotePropertyValue $CurrentHash
+        $DataHashes | ConvertTo-Json -Depth 10 | Out-File data.json
+    }
+}
